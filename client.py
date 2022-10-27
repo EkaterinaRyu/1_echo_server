3de2dd53e292a0a74466c22063ad8461142f4d7a
EkaterinaRyu <113510841+EkaterinaRyu@users.noreply.github.com>
2022-10-27 13:29:41 +0300

    finished project

--- client.py
+++ client.py
@@ -1,16 +1,67 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+def inity():
+    
+    print('initialization of the client...')
+    sock = socket.socket()
+    sock.setblocking(1)
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+    print('do you wanna enter IP-address? [yes/no]')
+    yn = input()
+    if yn == 'yes':
+        print('enter the IP address: ')
+        ad = str(input())
+    elif yn == 'no':
+        ad = 'localhost'
+    else:
+        print('youre an idiot')
+    
+    print('do you wanna enter port №? [yes/no]')
+    yn = input()
+    if yn == 'yes':
+        print('enter the port №: ')
+        pt = int(input())
+    elif yn == 'no':
+        pt = 9090
+    else:
+        print('youre an idiot')
+    print('connecting to {} {}...'.format(ad, pt))
+    
+    sock.connect((ad, pt))
+    
+    return sock
+
+sock = inity()
 
 data = sock.recv(1024)
+print(data.decode())
+if data.decode()[0] == 'p':
+    msg = input()
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    print(data.decode())
+
+while True:
+#    print('я вошел в цикл')
+    print('enter your message: ')
+    msg = input()
+    try: 
+        sock.send(msg.encode())
+        data = sock.recv(1024)
+        print(data.decode())    
+    except ConnectionResetError or ConnectionAbortedError:
+        print('The server didnt answer.')
+        break
+    if msg == 'exit' or msg == 'serverdie':
+        break
 
 sock.close()
 
 print(data.decode())
+
+
+
+"""
+проверка ввода в отдельную функцию (?),
+автоматическое изменение порта
+"""
--- server.py
+++ server.py
@@ -1,20 +1,71 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+from time import sleep
 
-msg = ''
+import logging
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+def logg(text):
+    logger = logging.getLogger()
+    handler = logging.FileHandler('logfile.log')
+    logger.addHandler(handler)
+    logger.error(text)
 
-print(msg)
 
-conn.close()
+def connection_to_client():
+
+    connected = False
+    while not connected:
+        try:
+            sock = socket.socket()
+            sock.bind(('', 9090))
+            sock.listen(1)
+            conn, addr = sock.accept()
+            logg('initialization of the server...')
+            logg('connection established: {}'.format(addr))
+            connected = True
+        except:
+            sock.close()
+            sleep(10)
+    return conn, addr
+                        
+def log_check(conn, addr):
+    t = False
+    ipe, sk = addr
+    with open(r'E:\Финашка\Алгоритмы\эхо-сервер\1_echo_server_002\loginsIPs.txt', 'r') as f:
+        for i in f.readlines():
+            i = i.split(':')
+            if i[0] == ipe:
+                conn.send('oh, i know you, {}'. format(i[1]).encode())
+                t = True
+        if t == False:
+            conn.send('please, enter your login:'.encode())
+            log = conn.recv(1024).decode()
+            print(log)
+            with open(r'E:\Финашка\Алгоритмы\эхо-сервер\1_echo_server_002\loginsIPs.txt', 'a+') as f:
+                f.write(ipe + ':' + log + '\n')
+            conn.send('hello, {}'.format(log).encode())
+       
+def main():
+    conn, addr = connection_to_client()
+    log_check(conn, addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            logg('no data recieved, closing the connection...')
+            break
+        msg = data.decode()
+        if msg == 'exit':
+            logg('closing the connection...')
+            conn.close()
+            conn, addr = connection_to_client()
+            log_check(conn, addr)
+            sleep(10)
+        elif msg == 'serverdie':
+            conn.close()
+            logg('killing the server...')
+            break
+        conn.send(data)
+        print(msg)
+        
+    
+main()
